c03cb273416c49725088e5b91403793f123e351e
eddielicaycay <eddielicaycay@gmail.com>
2024-01-16 15:04:26 -0800

    updating Readme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,12 @@ import airbnbLogo from '../images/airbnb-logo.png';
 
 const Navbar = () => {
   return (
+
     <nav>
       <img src={airbnbLogo} alt="airbnb logo" />
     </nav>
+    
   )
 }
 
-export default Navbar;  
+export default Navbar;
